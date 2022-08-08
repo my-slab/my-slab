e393ed3e13407bb4b02c88a1c749b7c43014a69c
Mitch Stewart <mitch.stewart@hey.com>
2022-08-08 23:56:53 +1000

    Create cycling.js

---
+++ src/components/cycling.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * @example
+ * <Cycling />
+ */
+export function Cycling(props) {
+  return (
+    <section>
+      <h4>🚵‍♂️ I love riding my bike</h4>
+      <p>I've been cycling</p>
+      <iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/25723202/latest-rides/31801716d18ee6741ccc1e826d8f779e29ff0fc9'></iframe>
+    </section>
+  )
+}
