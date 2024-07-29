df2fe52ed158e4db84f325b98396d158479598de
Mitch Stewart <mitch.stewart@hey.com>
2024-07-29 20:51:50 +1000

    Update readme.js

--- src/components/readme.js
+++ src/components/readme.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
-import { Movies } from './movies'
 import { Music } from './music'
 
 export function Readme(props) {
-  let { movies, music } = props
+  let { music } = props
 
   return (
     <article>
       <h3>I'm Mitch</h3>
       <section>
         <p>👨 he/him</p>
-        <p>📍 Naarm (Melbourne), Australia</p>
+        <p>📍 Melbourne, Australia</p>
       </section>
       <hr />
       <Music data={music} />
