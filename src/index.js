c6278f6d8ebb60f4dc379a586ed2845293e09296
Mitch Stewart <mitch.stewart@hey.com>
2024-08-04 13:45:30 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/server'
 import fs from 'fs'
 
 import { Readme } from './components'
-import { getMovies, getMusic, getPosts } from './api'
+import { getMusic } from './api'
 ;(async function () {
-  let movies = await getMovies()
   let music = await getMusic()
-  let blog = await getPosts()
 
   let markdown = ReactDOM.renderToStaticMarkup(
-    <Readme {...{ blog, movies, music }} />
+    <Readme {...{ music }} />
   )
 
   fs.writeFileSync('./README.md', markdown)
